4d2624525f6f0e38588b10d1228ddbf80bde5f27
Martin Whitman <martin.whitman.1776@gmail.com>
2019-04-14 21:34:09 -0500

    Contains token for API calls to iexcloud.io Requires x86 debug config in VS

--- C++ Stock App/C++ Stock App.cpp
+++ C++ Stock App/C++ Stock App.cpp
@@ -1,21 +1,17 @@
-// C++ Stock App.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Proof of API concept for Stock Quote app
+//Uses cpr (C++ Requests library) installed via VCPKG to execute API requests from iexcloud.io financial data api
+//requires x86 debug config in VS not x64
 
 #include "pch.h"
 #include <iostream>
+#include <cpr/cpr.h>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	cout << "Hello API!\n";
+
+	cout << "Current quote for Merck (NYSE MRK): ";
+	auto r = cpr::Get(cpr::Url{ "https://cloud.iexapis.com/beta/stock/MRK/quote/latestPrice?token=pk_05b60effc229410da18fd2dd0aa3bbae" });
+	cout << r.text;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
