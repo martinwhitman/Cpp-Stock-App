7ad53566e96b42886a93670cb60808299755efc0
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-04-21 16:02:53 -0500

    Addeds stock methods
    Added method returning more stock information, takes the stock name, returns name, calculation price, latest price.
    Commented everything out.

--- C++ Stock App/C++ Stock App.cpp
+++ C++ Stock App/C++ Stock App.cpp
@@ -14,7 +14,7 @@ using namespace std;
 using namespace rapidjson;
 
 int getStockPrice();
-int test();
+int getStockInfo();
 
 int request;
 const int MOSTREQUESTS = 100;
@@ -27,106 +27,102 @@ int main()
 	int age;
 	int request;
 
+	cout << "Stock Application - This app will give you options to get information for specific stocks." << endl << endl;
 	cout << "What is your name? " << endl;
-	cin >> name;
+	cin >> name;									//Gets the users name and stores it in a new user object
 
 	User user1;
 	user1.setName(name);
 
-	cout << "How old are you? " << endl;
+	cout << "How old are you? " << endl;			//Gets the users age, if the user is not older than 18, they cannot use the application
 	cin >> age;
 	user1.setAge(age);
 
-	while (choice != 9 && user1.getRequests() < MOSTREQUESTS)
-	{
-		cout << "What would you like to do?" << endl;
-		cout << "1. Get Stock Price" << endl;
-		cout << "2. Something else" << endl;
-		cout << "3. test " << endl;
-		cout << "9. Exit Program" << endl;
+	if (user1.getAge() >= 18) {
+		while (choice != 9 && user1.getRequests() < MOSTREQUESTS)		//if the user hits 100 requests the application will end
+		{
+			cout << "What would you like to do?" << endl;				//menu for the application
+			cout << "1. Get Stock Price" << endl;
+			cout << "2. Get All Stock Info" << endl;
+			cout << "9. Exit Program" << endl;
 
+			cin >> choice;												//gets the users choice
 
-		cin >> choice;
-
-		if (choice == 1)
-		{
-			request = getStockPrice();
-			user1.setRequests(request);
-		}
-		else if (choice == 2)
-		{
-			request = test();
-			user1.setRequests(request);
-		}
-		else if (choice == 3)
-		{
-			request = getStockPrice();
-			user1.setRequests(request);
-		}
-		else if (choice == 9)
-		{
-			break;
+			if (choice == 1)											//depending on user input will get different information
+			{	
+				request = getStockPrice();								//depending on the request the request amount will go up
+				user1.setRequests(request);
+			}
+			else if (choice == 2)
+			{
+				request = getStockInfo();
+				user1.setRequests(request);
+			}
+			else if (choice == 9)
+			{
+				break;
+			}
 		}
 	}
+	else if (user1.getAge() < 18)
+	{
+		cout << "Sorry, you are too young to use this application. Please come back when you are of age." << endl;
+	}
 
-	cout << user1.getRequests();
+	
+	cout << endl << "Ending Requests: " << user1.getRequests() << endl;				//Tells the user how many requests were made
 
 	return 0;
 }
 
-int getStockPrice()
+int getStockPrice()										//just gets the stock price -- uses the rapidjson method
 {
-	string str;
+	string link;										//Declares the strings needed to make the call
 	string userNum = "";
 	string stock1 = "https://cloud.iexapis.com/beta/stock/";
 	string stock2 = "/quote/latestPrice?token=pk_05b60effc229410da18fd2dd0aa3bbae";
 
 	cout << "Please input stock name:  ";
+	cin >> userNum;										//gets user input for the stock
 
-	cin >> userNum;
+	link = stock1 + userNum + stock2;
 
+	auto r = cpr::Get(cpr::Url{ link });				//calls to a website and returns a json string
+	cout << endl << r.text << endl << endl;
 
-	str = stock1 + userNum + stock2;
-
-
-	auto r = cpr::Get(cpr::Url{ str });
-	cout << endl << r.text;
-
-	return 1;
+	return 1;											//returns 1 for 1 request
 }
 
-int test()
+int getStockInfo()										//gets more stock info -- uses rapidjson method
 {
-	string start = "https://cloud.iexapis.com/beta/stock/";
+	string startUrl = "https://cloud.iexapis.com/beta/stock/";			//declares urls
 	string uInput;
-	string fin = "/quote/?token=pk_05b60effc229410da18fd2dd0aa3bbae";
+	string endUrl = "/quote/?token=pk_05b60effc229410da18fd2dd0aa3bbae";
 	string link;
 
-	cout << "Get Stock" << endl;
+	cout << endl << "Get Stock" << endl;								//gets user input for stock info
 	cin >> uInput;
-
-	link = start + uInput + fin;
-
+		
+	link = startUrl + uInput + endUrl;									//creates the link and makes the call
 	auto r = cpr::Get(cpr::Url{ link });
-
 	std::string s = r.text;
 
 	int n = s.length();
-
 	char json[1000];
+	strcpy_s(json, s.c_str());											//parses the json string to char array
 
-	strcpy_s(json, s.c_str());
-
-	Document d;
+	Document d;															//creates a document and parses the array to the document
 	d.Parse(json);
+	Value& compName = d["companyName"];									//creates a value for the corresponding info
+	Value& calcPrice = d["calculationPrice"];
+	Value& lat = d["latestPrice"];
 
-	Value& c = d["companyName"];
-	cout << endl;
-
-	cout << c.GetString() << endl;
-
-	cout << endl;
-
+	cout << endl << "Company Name: " << compName.GetString() << endl;	//lists the information for the stock chosen
+	cout << "Calc Price: " << calcPrice.GetString() << endl;
+	cout << "Latest Price: " << lat.GetDouble() << endl;
+		
+	/*  This code included for testing, will return the full json string to see if there is
+		an unexpected value returned, like null
 	// 3. Stringify the DOM
 	StringBuffer buffer;
 	Writer<StringBuffer> writer(buffer);
@@ -134,6 +130,7 @@ int test()
 
 	// Output the full json
 	std::cout << buffer.GetString() << std::endl;
+	*/
 
-	return 12;
+	return 12;															//returns the request number
 }
